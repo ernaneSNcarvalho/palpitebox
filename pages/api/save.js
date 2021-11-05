803695ed5de4d0da714a55de9969eeecf819af41
ernane <ernanecarvalho89@gmail.com>
2021-11-04 22:13:33 -0300

    Projeto finalizado

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,6 +1,5 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 import moment from 'moment'
-import credentials from '../../credentials.json'
 import { fromBase64 } from '../../utils/base64'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
